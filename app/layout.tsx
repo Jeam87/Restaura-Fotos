c5b38d7554c00315195738adc5eee10057f1e397
Jeam87 <88300558+Jeam87@users.noreply.github.com>
2026-09-02 23:18:34 -0600

    Simplify RootLayout function signature

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,1 @@
-export default function RootLayout({children}:{children:React.ReactNode}){
- return <html lang="es"><body style={{margin:0,background:"black"}}>{children}</body></html>
-}
+export default function RootLayout({children}){return <html lang="es"><body style={{margin:0,background:"black"}}>{children}</body></html>}
